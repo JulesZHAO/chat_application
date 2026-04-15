c8fbdf5e5ef5f8bdcd2c4237b2b0fbbff2d556e4
Zhenyu Zhao <zhenyu.zhao@etu.utc.fr>
2026-04-16 00:58:09 +0200

    add controller

---
+++ project/src/main/java/com/sr03/chat/controller/CanalController.java
@@ -0,0 +1,33 @@
+package com.sr03.chat.controller;
+
+import com.sr03.chat.model.Canal;
+import com.sr03.chat.model.Utilisateur;
+import com.sr03.chat.repository.CanalRepository;
+import com.sr03.chat.repository.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/canaux")
+public class CanalController {
+
+    @Autowired
+    private CanalRepository canalRepository;
+
+    @PostMapping
+    public Canal planifierCanal(@RequestBody Canal canal) {
+        return canalRepository.save(canal);
+    }
+
+    @GetMapping("/proprietaire/{id}")
+    public List<Canal> getCanauxParPropietaire(@PathVariable Long id) {
+        return canalRepository.findByProprietaireId(id);
+    }
+
+    @GetMapping("/invite/{id}")
+    public List<Canal> getCanauxParInvite(@PathVariable Long id) {
+        return canalRepository.findByInvitesId(id);
+    }
+}
