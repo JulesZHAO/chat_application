03d5cf256ef34e6f34dc6d0a87d73662fd0ad9ad
Gaspard <gaspard.petri@etu.utc.fr>
2026-05-03 17:24:56 +0200

    ajout layout et acceuil html + controller admin

---
+++ project/src/main/java/com/sr03/chat/controller/AdminController.java
@@ -0,0 +1,24 @@
+package com.sr03.chat.controller;
+
+import com.sr03.chat.repository.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
+    @GetMapping("/admin")
+    public String afficherAccueil(Model model) {
+        // On récupère tous les utilisateurs depuis la base H2
+        // "model" permet de transporter cette liste vers le fichier HTML
+        model.addAttribute("utilisateurs", utilisateurRepository.findAll());
+
+        // On dit à Spring d'afficher le fichier templates/accueil.html
+        return "accueil";
+    }
+}
